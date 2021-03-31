ba3f78900b99291db43da03780a8a502ddb312db
Gabriela Tavares <75052113+gasatabi@users.noreply.github.com>
2021-03-31 18:33:39 -0300

    Versão final

--- src/test/java/io/cucumber/guardiasDaQualidade/steps/CasoDeTeste1.java
+++ src/test/java/io/cucumber/guardiasDaQualidade/steps/CasoDeTeste1.java
@@ -1,6 +1,9 @@
 package io.cucumber.guardiasDaQualidade.steps;
 
 import org.openqa.selenium.WebElement;
+
+import static org.junit.Assert.assertEquals;
+
 import org.openqa.selenium.By;
 
 import io.cucumber.java.pt.*;
@@ -13,23 +16,48 @@ public class CasoDeTeste1 extends Configuracao {
     public void deve_fechar_a_caixa_de_informacao() {
 		WebElement input = browser.findElement(By.cssSelector(".onetrust-close-btn-handler.onetrust-close-btn-ui.banner-close-button.ot-close-icon"));
 		input.click();
-		Configuracao.fechar();
+		fechar();
 	}
-
-    @Quando("clico em {string}")
-	public void clico_em(String string) {
-		WebElement cookies = browser.findElement(By.cssSelector(".cookie-setting-link"));
-		cookies.click();
-	}
-
-    @Entao("devo ver o item de {string}")
-    public void devo_ver_o_item_de(String string) {
-        browser.findElements(By.cssSelector(".ot-sdk-eight.ot-sdk-columns.description-container"));    
+    
+    @Dado("clico em configuracoes de cookie")
+    public void clico_em_configuracoes_de_cookie() {
+        browser.findElement(By.cssSelector(".cookie-setting-link")).click();
+    }
+    
+    @Entao("devo ver o item de sua privacidade {string}")
+    public void devo_ver_o_item_de_sua_privacidade(String string) {
+        WebElement str0 = Configuracao.browser.findElement(By.cssSelector("#privacy-text"));
+        assertEquals(string, str0.getText());
     }
 
-    @Entao("devo ver as opcoes")
-    public void devo_ver_as_opcoes(io.cucumber.datatable.DataTable dataTable) {
-        Configuracao.browser.findElements(By.cssSelector(".ot-main-content.pc-content"));
-        Configuracao.fechar();
+    @Entao("devo ver Cookies estritamente necessarias {string}")
+    public void devo_ver_Cookies_estritamente_necessarias(String string) {
+        WebElement str1 = Configuracao.browser.findElement(By.cssSelector("#ot-header-id-1"));
+        assertEquals(string, str1.getText());
+    }
+
+    @Entao("devo ver Cookies Analiticos de Primeira Parte {string}")
+    public void devo_ver_Cookies_Analiticos_de_Primeira_Parte(String string) {
+        WebElement str2 = Configuracao.browser.findElement(By.cssSelector("#ot-header-id-2"));
+        assertEquals(string, str2.getText());
+    }
+    
+    @Entao("devo ver Cookies de Desempenho e Cookies Funcionais {string}")
+    public void devo_ver_Cookies_de_Desempenho_e_Cookies_Funcionais(String string) {
+        WebElement str3 = Configuracao.browser.findElement(By.cssSelector("#ot-header-id-3"));
+        assertEquals(string, str3.getText());
+    }
+
+    @Entao("devo ver Cookies de Publicidade e Redes Sociais {string}")
+    public void devo_ver_Cookies_de_Publicidade_e_Redes_Sociais(String string) {
+        WebElement str4 = Configuracao.browser.findElement(By.cssSelector("#ot-header-id-4"));
+        assertEquals(string, str4.getText());
+    } 
+
+    @Entao("clico no botao confirmar")
+    public void clico_no_botao_confirmar() {
+        WebElement botao = Configuracao.browser.findElement(By.cssSelector("#onetrust-pc-sdk > div.ot-button-group-parent > div.ot-button-group > button.save-preference-btn-handler.onetrust-close-btn-handler"));
+        botao.click();
+        fechar();
     }
 }
--- src/test/resources/io/cucumber/guardiasDaQualidade/features/CasoDeTeste1.feature
+++ src/test/resources/io/cucumber/guardiasDaQualidade/features/CasoDeTeste1.feature
@@ -10,10 +10,10 @@ Cenario: Aceitar o cookie LGPD
 Entao deve fechar a caixa de informacao
 
 Cenario: Configuracoes do cookie
-E clico em "configuracao dos cookies"
-Entao devo ver o item de "sua privacidade"
-E devo ver as opcoes
-| Cookies estritamente necessárias           |
-| Cookies Analíticos de Primeira Parte       |
-| Cookies de Desempenho e Cookies Funcionais |
-| Cookies de Publicidade e Redes Sociais     |
+E clico em configuracoes de cookie
+Entao devo ver o item de sua privacidade "Sua privacidade"
+E devo ver Cookies estritamente necessarias "Cookies estritamente necessárias"
+E devo ver Cookies Analiticos de Primeira Parte "Cookies Analíticos de Primeira Parte"
+E devo ver Cookies de Desempenho e Cookies Funcionais "Cookies de Desempenho e Cookies Funcionais"
+E devo ver Cookies de Publicidade e Redes Sociais "Cookies de Publicidade e Redes Sociais"
+Entao clico no botao confirmar
